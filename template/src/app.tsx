a32cb2b82576e60ba1c4924aa1ec25b1eae5bbfe
florent fauchille <florentfauchille@florents-macbook-pro.home>
2020-09-25 18:27:41 +0200

    WIP: boilerplate

---
+++ template/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App: React.FC = () => (
+  <h1>Hello Lucas!</h1>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
